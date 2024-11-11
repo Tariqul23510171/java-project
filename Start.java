aea6ad4eb3e87db92058e841240db2e190843219
Tariqul23510171 <md.tariqul.official@gmail.com>
2024-11-11 23:53:16 +0600

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,16 @@
+import java.lang. * ;
+
+//import Entity.*;
+import GUI.*;
+public class Start
+{
+  public static void main(String args[])
+  {
+	   Info T = new Info();
+	   T.setVisible(true);
+
+
+
+  }
+
+}
